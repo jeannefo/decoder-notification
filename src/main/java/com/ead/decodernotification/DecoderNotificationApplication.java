b179152becb86cca8311a7d5492bfa8d5d9cb933
jeannefo <jeannefoliveira@gmail.com>
2023-03-04 14:18:59 -0300

    criando o projeto

---
+++ src/main/java/com/ead/decodernotification/DecoderNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.ead.decodernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoderNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoderNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ead/decodernotification/DecoderNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.decodernotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
